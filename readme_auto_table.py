90f307da20fde590757a8af1ea7e6946d094d8ed
Natsec <kamilp@hotmail.fr>
2020-08-26 15:14:43 +0200

    group ok

--- readme_auto_table.py
+++ readme_auto_table.py
@@ -3,6 +3,13 @@ import re
 vars_file = "vars\main.yml"
 readme_file = "README.md"
 
+with open(vars_file, "r", encoding='utf-8') as f:
+	vars_txt = f.read()
 
 regex_table = '## Actions\n(.+)\n##' # single line flag
-regex_vars = '# Setting syntax\s+: (?P<syntax>.+)\n# Description\s+: (?P<desc>.+)\n# Apache default\s+: (?P<apache>.+)\n(?P<armor>.+): (?P<armor_value>.+)'
+regex_vars = '# Setting syntax\s+: (?P<syntax>.+)\n# Description\s+: (?P<desc>.+)\n# Apache default\s+: (?P<apache>.+)\n(?P<armor_name>.+): (?P<armor_value>.+)'
+
+match = re.finditer(regex_vars, vars_txt)
+if match:
+	for m in match:
+		print("{} | {} | {} | {} | {}".format(m.group("syntax"), m.group("desc"), m.group("apache"), m.group("armor_name"), m.group("armor_value")))
